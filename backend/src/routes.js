ec531363deab6c73e46c6a1b59219ef660c1fa52
Fabiano Souza <fabianohensouza@gmail.com>
2020-01-18 19:33:45 -0300

    Application Backend Created realized at: Sat Jan 18

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,16 +1,12 @@
 const { Router } = require('express');
-const axios = require('axios');
+const DevController = require('./controllers/DevController');
+const SearchController = require('./controllers/SearchController');
 
 const routes = Router();
 
-routes.post('/devs',  async (request, response) => {
-    const { github_username } =request.body;
+routes.get('/devs', DevController.index);
+routes.post('/devs', DevController.store);
 
-    const apiResponse = await axios.get(`https://api.gitnuh.com/users/${github_username}`);
-
-    console.log(apiResponse.data);
-
-    return response.json({ message:'Hello World Omnistack!'});
-});
+routes.get('/search', SearchController.index);
 
 module.exports = routes;
