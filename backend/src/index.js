17785da7d479b4e7438a9fb7006a604c12f44970
Fabiano Souza <fabianohensouza@gmail.com>
2020-01-21 12:46:15 -0300

    Biblioteca Cors incluída a pedido de membro da mentoria realized at: Tue Jan 21

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const routes = require('./routes');
+const http = require('http');
 
 const app = express();
+const server = http.Server(app);
 
 mongoose.connect('mongodb+srv://omnistack:omnistackweek@cluster0-iayyk.mongodb.net/week10?retryWrites=true&w=majority', {
     useNewUrlParser: true,
@@ -15,6 +18,7 @@ mongoose.connect('mongodb+srv://omnistack:omnistackweek@cluster0-iayyk.mongodb.n
 // Query Params, Route Params and Body
 
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
