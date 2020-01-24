0c54840387064d7182fc53c7b567cf8d554615f2
Fabiano Souza <fabianohensouza@gmail.com>
2020-01-24 12:46:15 -0300

    Styling the react frontend realized at: Fri Jan 24

--- web/src/App.js
+++ web/src/App.js
@@ -1,10 +1,104 @@
 import React, {useState} from 'react';
 
+import './global.css';
+import './app.css';
+import './sidebar.css';
+import './main.css';
+
 function App() {
 
   return (
     <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <form>
+          <div className="input-block">
+            <label htmlFor="username_github">Usuário do Github</label>
+            <input name="username_github" id="username_github" riquired/>
+          </div>
+          
+          <div className="input-block">
+            <label htmlFor="techs">Tecnologias</label>
+            <input name="techs" id="techs" riquired/>
+          </div>
 
+          <div className="input-group">
+            <div className="input-block">
+              <label htmlFor="latitude">Latitude</label>
+              <input name="latitude" id="latitude" riquired/>
+            </div>
+            <div className="input-block">
+              <label htmlFor="longitude">Longitude</label>
+              <input name="longitude" id="longitude" riquired/>
+            </div>
+          </div>
+
+          <button type="submit">Salvar</button>
+          
+        </form>
+      </aside>
+      <main>
+        <ul>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars1.githubusercontent.com/u/33223817?s=460&v=4" alt="Fabiano Souza"/>
+                <div className="user-info">
+                  <strong>Fabiano Souza</strong>
+                  <span>AWS, Linux, JS</span>
+                </div>
+            </header>
+            <p>
+              Teste do Teste Testado!
+            </p>
+            <a href="https://github.com/fabianohensouza/">Acessar Perfil no Github</a>
+          </li>
+        </ul>
+        <ul>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars1.githubusercontent.com/u/33223817?s=460&v=4" alt="Fabiano Souza"/>
+                <div className="user-info">
+                  <strong>Fabiano Souza</strong>
+                  <span>AWS, Linux, JS</span>
+                </div>
+            </header>
+            <p>
+              Teste do Teste Testado!
+            </p>
+            <a href="https://github.com/fabianohensouza/">Acessar Perfil no Github</a>
+          </li>
+        </ul>
+        <ul>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars1.githubusercontent.com/u/33223817?s=460&v=4" alt="Fabiano Souza"/>
+                <div className="user-info">
+                  <strong>Fabiano Souza</strong>
+                  <span>AWS, Linux, JS</span>
+                </div>
+            </header>
+            <p>
+              Teste do Teste Testado!
+            </p>
+            <a href="https://github.com/fabianohensouza/">Acessar Perfil no Github</a>
+          </li>
+        </ul>
+        <ul>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars1.githubusercontent.com/u/33223817?s=460&v=4" alt="Fabiano Souza"/>
+                <div className="user-info">
+                  <strong>Fabiano Souza</strong>
+                  <span>AWS, Linux, JS</span>
+                </div>
+            </header>
+            <p>
+              Teste do Teste Testado!
+            </p>
+            <a href="https://github.com/fabianohensouza/">Acessar Perfil no Github</a>
+          </li>
+        </ul>
+      </main>
     </div>
   );
 }
