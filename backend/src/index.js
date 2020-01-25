9ac682b9b2c814a1db22c35fc861abe1d9a1d3d9
Fabiano Souza <fabianohensouza@gmail.com>
2020-01-25 19:54:08 -0300

    Created the code to insert new developers in the database realized at: Sat Jan 25

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,10 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const routes = require('./routes');
-const http = require('http');
 
 const app = express();
-const server = http.Server(app);
 
 mongoose.connect('mongodb+srv://omnistack:omnistackweek@cluster0-iayyk.mongodb.net/week10?retryWrites=true&w=majority', {
     useNewUrlParser: true,
--- web/src/App.js
+++ web/src/App.js
@@ -1,4 +1,5 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
+import api from './services/api';
 
 import './global.css';
 import './app.css';
@@ -6,30 +7,103 @@ import './sidebar.css';
 import './main.css';
 
 function App() {
+  const [devs, setDevs] = useState([]);
+
+  const [github_username, setGithubUsername] = useState('');
+  const [techs, setTechs] = useState('');
+
+  const [latitude, setLatitude] = useState('');
+  const [longitude, setLongitude] = useState('');
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const { latitude, longitude } = position.coords;
+
+        setLatitude(latitude);
+        setLongitude(longitude);
+      },
+      (err) => {
+        console.log(err);
+      },
+      {
+        timeout: 30000,
+      }
+    );
+  }, []);
+
+  useEffect(() => {
+    async function loadDevs() {
+        const response = await api.get('/devs')
+
+        setDevs(response.data)
+    }
+
+    loadDevs()
+  }, [])
+
+  async function handleAddDev(e) {
+    e.preventDefault();
+
+    const response = await api.post('/devs', {
+      github_username,
+      techs,
+      latitude,
+      longitude
+    })
+
+    setGithubUsername('');
+    setTechs('');
+  }
 
   return (
     <div id="app">
       <aside>
         <strong>Cadastrar</strong>
-        <form>
+        <form onSubmit={handleAddDev}>
           <div className="input-block">
             <label htmlFor="username_github">Usuário do Github</label>
-            <input name="username_github" id="username_github" riquired/>
+            <input 
+              name="username_github" 
+              id="username_github"
+              riquired
+              value={github_username}
+              onChange={ e => setGithubUsername(e.target.value)}
+            />
           </div>
           
           <div className="input-block">
             <label htmlFor="techs">Tecnologias</label>
-            <input name="techs" id="techs" riquired/>
+            <input 
+              name="techs" 
+              id="techs" 
+              riquired
+              value={techs}
+              onChange={ e => setTechs(e.target.value)}
+            />
           </div>
 
           <div className="input-group">
             <div className="input-block">
               <label htmlFor="latitude">Latitude</label>
-              <input name="latitude" id="latitude" riquired/>
+              <input 
+                type="number" 
+                name="latitude" 
+                id="latitude" 
+                riquired 
+                value={latitude}
+                onChange={ e => setLatitude(e.target.value)}
+              />
             </div>
             <div className="input-block">
               <label htmlFor="longitude">Longitude</label>
-              <input name="longitude" id="longitude" riquired/>
+              <input type="number" 
+                name="longitude" 
+                id="longitude" 
+                riquired 
+                value={longitude}
+                onChange={ e => setLongitude(e.target.value)}
+              />
             </div>
           </div>
 
@@ -39,6 +113,7 @@ function App() {
       </aside>
       <main>
         <ul>
+          {devs.map}
           <li className="dev-item">
             <header>
               <img src="https://avatars1.githubusercontent.com/u/33223817?s=460&v=4" alt="Fabiano Souza"/>
@@ -52,51 +127,7 @@ function App() {
             </p>
             <a href="https://github.com/fabianohensouza/">Acessar Perfil no Github</a>
           </li>
-        </ul>
-        <ul>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars1.githubusercontent.com/u/33223817?s=460&v=4" alt="Fabiano Souza"/>
-                <div className="user-info">
-                  <strong>Fabiano Souza</strong>
-                  <span>AWS, Linux, JS</span>
-                </div>
-            </header>
-            <p>
-              Teste do Teste Testado!
-            </p>
-            <a href="https://github.com/fabianohensouza/">Acessar Perfil no Github</a>
-          </li>
-        </ul>
-        <ul>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars1.githubusercontent.com/u/33223817?s=460&v=4" alt="Fabiano Souza"/>
-                <div className="user-info">
-                  <strong>Fabiano Souza</strong>
-                  <span>AWS, Linux, JS</span>
-                </div>
-            </header>
-            <p>
-              Teste do Teste Testado!
-            </p>
-            <a href="https://github.com/fabianohensouza/">Acessar Perfil no Github</a>
-          </li>
-        </ul>
-        <ul>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars1.githubusercontent.com/u/33223817?s=460&v=4" alt="Fabiano Souza"/>
-                <div className="user-info">
-                  <strong>Fabiano Souza</strong>
-                  <span>AWS, Linux, JS</span>
-                </div>
-            </header>
-            <p>
-              Teste do Teste Testado!
-            </p>
-            <a href="https://github.com/fabianohensouza/">Acessar Perfil no Github</a>
-          </li>
+          
         </ul>
       </main>
     </div>
